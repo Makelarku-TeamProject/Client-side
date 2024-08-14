5511e03905bf920a4c9256353ee51f321524e3ef
Azzasafah <85096618+Akanerd@users.noreply.github.com>
2024-08-14 23:09:55 +0700

    test

--- src/components/customer/SectionCampaign/SectionCampaign.jsx
+++ src/components/customer/SectionCampaign/SectionCampaign.jsx
@@ -3,7 +3,7 @@ import { DataContext } from '../../../context/DataContext';
 
 const SectionCampaign = () => {
   const { sliders, sliderLoading, sliderError, fetchSliders } = useContext(DataContext);
-  console.log('a')
+  console.log('ab')
   useEffect(() => {
     fetchSliders();
   }, [fetchSliders]);
